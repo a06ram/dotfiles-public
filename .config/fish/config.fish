71cb4fdd2448e11eb2fafe563d73be5f8ebdcee2
Alan Mendoza <alanmendoza9801@outlook.com>
2026-02-26 20:18:00 -0600

    Se refactoriza configuracion de fish shell y se agrega soporte de mouse en tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,6 @@
 # Clear fish shell welcome message
 set fish_greeting ""
 
-# Fish colors
-# set -g fish_color_param 2AA198
-# set -g fish_color_redirection 2AA198
-# set -g fish_color_quote B58900
-# set -g fish_color_comment DC322F
-# set -g fish_color_autosuggestion 808080
-# set -g fish_color_end 859900
-# set -g fish_color_cwd 859900
-# set -g __fish_git_prompt_color_cleanstate 859900
-# set -g __fish_git_prompt_color_dirtystate 268BD2
-# set -g __fish_git_prompt_color_stagedstate B58900
-# set -g __fish_git_prompt_color_branch 6C71C4 --bold
-
 # Aliases
 alias g git
 command -qv nvim && alias vim nvim
