f2bd762602dea4ea404b3b7c1095c207d1713fee
Alan Mendoza <alanmendoza9801@outlook.com>
2026-03-05 12:14:42 -0600

    Se agregan formatters para fish y sh en Neovim

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -3,8 +3,10 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	require("conform").setup({
 		formatters_by_ft = {
+			fish = { "fish_indent" },
 			lua = { "stylua" },
 			python = { "black" },
+			sh = { "shfmt" },
 		},
 		format_on_save = {
 			lsp_fallback = true,
