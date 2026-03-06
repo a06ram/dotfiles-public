c2d050c76bbbb94819e9b57e1eabd34053d660fa
Alan Mendoza <alanmendoza9801@outlook.com>
2026-03-06 12:51:11 -0600

    Se refactorizan opciones que no necesarias para mi en neovim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -31,8 +31,6 @@ vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
