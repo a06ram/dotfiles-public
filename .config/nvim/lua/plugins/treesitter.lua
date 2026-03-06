b72f7a4a278a9034cf0386f872280f0e2add7cc3
Alan Mendoza <alanmendoza9801@outlook.com>
2026-03-06 12:32:50 -0600

    Se busca mejorar el syntax highlight para archivos tipo markdown en neovim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
 	-- Syntax highlight
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
 		build = ":TSUpdate",
 		opts = {
 			ensure_installed = {
@@ -11,6 +12,14 @@ return {
 				"fish",
 				"gitignore",
 				"make",
+			},
+			ignore_install = { "markdown", "markdown-inline" },
+			auto_install = false,
+			highlight = {
+				disable = {
+					"markdown",
+					"markdown-inline",
+				},
 			},
 		},
 	},
