0e9a69655d63b03e0907745f104d152215bd3c1a
Alan Mendoza <alanmendoza9801@outlook.com>
2026-03-23 18:31:47 -0600

    Se configuran variables de entorno para trabajar con Java

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,6 +13,8 @@ end
 
 # Neovim as default editor
 set -gx EDITOR nvim
+set -Ux JAVA_HOME /usr/lib/jvm/java-25-openjdk
+set -Ux MAVEN_HOME /opt/maven/
 
 # Fzf
 set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
@@ -21,5 +23,7 @@ set -g FZF_LEGACY_KEYBINDINGS 0
 
 fish_add_path ~/.local/bin
 fish_add_path ~/.node_modules/bin
+fish_add_path $JAVA_HOME/bin $PATH
+fish_add_path $MAVEN_HOME/bin $PATH
 fish_add_path ~/.pyenv/bin
 pyenv init - | source
