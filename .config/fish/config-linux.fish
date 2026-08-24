cfbe31ea203f992dc5a43dc443fe9d68783d3155
Alan Mendoza <alanmendoza9801@outlook.com>
2026-08-24 15:37:39 -0600

    Se refactoriza y documenta el codigo de configuracion de fish shell

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -1,7 +1,8 @@
-
+# Java setup
 set -Ux JAVA_HOME /usr/lib/jvm/java-25-openjdk
 set -Ux MAVEN_HOME /opt/maven/
 
+# Path configuration
 fish_add_path ~/.local/bin
 fish_add_path ~/.node_modules/bin
 fish_add_path $JAVA_HOME/bin $PATH
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,20 +13,11 @@ end
 
 # Neovim as default editor
 set -gx EDITOR nvim
-#set -Ux JAVA_HOME /usr/lib/jvm/java-25-openjdk
-#set -Ux MAVEN_HOME /opt/maven/
 
 # Fzf
 set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
 set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
 set -g FZF_LEGACY_KEYBINDINGS 0
-
-#fish_add_path ~/.local/bin
-#fish_add_path ~/.node_modules/bin
-#fish_add_path $JAVA_HOME/bin $PATH
-#fish_add_path $MAVEN_HOME/bin $PATH
-#fish_add_path ~/.pyenv/bin
-#pyenv init - | source
 
 # Selects the correct Fish shell configuration for your env
 switch (uname)
